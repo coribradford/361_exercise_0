f287034b93c5531cdafb3409201fa7311b47c479
cori <bradfcor@oregonstate.edu>
2021-09-25 17:47:41 -0700

    fixed print, working correctly

--- warmup.py
+++ warmup.py
@@ -1,22 +1,20 @@
 # Corinne Bradford
-# 9/22/2021
+# 9/25/2021
 # Fall 361
 
 with open("CS361_Exercise_0_2021_f1_Drivers.txt") as file:
     data = file.readlines()
 
 for i in range(len(data)):
-    data[i] = data[i].splitlines(keepends=True)
+    data[i] = data[i].split()
 
 def pretty_print(data, style):
-    print("2021 F1 Drivers - ", style)
+    print(" \n2021 F1 Drivers - ", style)
     print("=========================")
     for i in range(len(data)):
         for j in range(len(data[i])):
-            print(data[i][j].strip(), end="\n")
-
-print(data)
-pretty_print(data, "test")
+            print(data[i][j], end=" ")
+        print("")
 
 alpha = sorted(data, key = lambda x: x[1])
 num = sorted(data, key = lambda x: int(x[2]))
